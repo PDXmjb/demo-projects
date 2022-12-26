ecb3b4170574c67e956a823527da8f40254c1589
Mike Brooks <35354129+PDXmjb@users.noreply.github.com>
2022-12-26 14:52:10 -0800

    (fix) Fetch - basic fetch

--- pages/demo/basic-api/index.tsx
+++ pages/demo/basic-api/index.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
 const BasicAPI = () => {
+  const [data, setData] = useState('');
+  useEffect(() => {
+    fetch('www.themealdb.com/api/json/v1/1/search.php?s=pizza').then(
+      (response) => console.log(response)
+    );
+    //.then((jsonResponse) => setData(jsonResponse))
+    //.catch((reason) => console.error(reason));
+  }, []);
+
+  console.log(data);
+
   return (
     <>
       <h2>Basic API - coming soon</h2>
       <p>This page shows a basic example of fetching data from an API.</p>
+      <p>{data}</p>
     </>
   );
 };
