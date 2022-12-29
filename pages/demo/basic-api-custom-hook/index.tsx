25c47f6ab289d738ad8ca2161966e2f921bad134
Mike Brooks <35354129+PDXmjb@users.noreply.github.com>
2022-12-29 15:42:38 -0800

    (fix) Styles update

--- pages/demo/basic-api-custom-hook/index.tsx
+++ pages/demo/basic-api-custom-hook/index.tsx
@@ -10,13 +10,7 @@ const BasicAPICustomHook = () => {
         This page shows a basic example of fetching data from an API using a
         custom hook.
       </p>
-      <div
-        style={{
-          border: '1px solid gray',
-          borderRadius: '16px',
-          padding: '16px',
-        }}
-      >
+      <div>
         <p>{data?.['full_name']}</p>
         <p>{data?.description}</p>
       </div>
