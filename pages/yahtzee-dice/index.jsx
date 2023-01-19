a081a4d9b28489099e518e3fecbd5a540565fdd0
Mike Brooks <35354129+PDXmjb@users.noreply.github.com>
2023-01-19 10:03:53 -0800

    (feat) adding improvements

--- pages/yahtzee-dice/index.jsx
+++ pages/yahtzee-dice/index.jsx
@@ -1,18 +1,32 @@
-import { Children, useEffect, useState } from 'react';
+import { Children, useCallback, useEffect, useState } from 'react';
 import styles from './yahtzee.module.scss';
+
+const rollDice = (numDice) => {
+  let tempArray = [];
+  for (let i = 0; i < numDice; i++) {
+    tempArray.push(Math.ceil(Math.random() * 6));
+  }
+
+  return tempArray.sort((a, b) => a - b);
+};
 
 const YahtzeeDice = () => {
   const [dice, setDice] = useState([]);
   const [numDice, setNumDice] = useState(6);
   const [heldDice, setHeldDice] = useState([]);
+  const [rollCount, setRollCount] = useState(0);
+  const [reset, setReset] = useState(false);
 
   useEffect(() => {
-    let tempArray = [];
-    for (let i = 0; i < numDice; i++) {
-      tempArray.push(Math.ceil(Math.random() * 6));
-    }
-    setDice(tempArray);
-  }, [numDice]);
+    setDice(rollDice(numDice));
+    setHeldDice([]);
+    setReset(false);
+    setRollCount(0);
+  }, [numDice, reset]);
+
+  const handleKeypress = (event) => {
+    console.log(event);
+  };
 
   const dieClick = (index) => {
     const tempArray = [...dice];
@@ -28,19 +42,24 @@ const YahtzeeDice = () => {
     setHeldDice([...tempArray]);
   };
 
-  const reRollHandler = () => {
-    let tempArray = [];
-    for (let i = 0; i < dice.length; i++) {
-      tempArray.push(Math.ceil(Math.random() * 6));
-    }
-    setDice(tempArray);
+  const rerollHandler = () => {
+    setDice(rollDice(dice.length));
+    setRollCount((count) => count + 1);
+  };
+
+  const takeNumber = (value) => {
+    const addToHeld = [...dice].filter((current) => current === value);
+    const tempArray = [...dice].filter((current) => current !== value);
+    setHeldDice((held) => [...held, ...addToHeld].sort((a, b) => a - b));
+    setDice([...tempArray]);
   };
 
   return (
-    <>
-      <div>
-        <label>Number of Dice: </label>
+    <div className={styles.yahtzee}>
+      <div className={styles.input}>
+        <label htmlFor="dice-input">Number of Dice: </label>
         <input
+          id="dice-input"
           type="number"
           onChange={(event) => setNumDice(event.target.value)}
           value={numDice}
@@ -55,8 +74,13 @@ const YahtzeeDice = () => {
           ))
         )}
       </div>
-      <button onClick={reRollHandler}>Re-roll</button>
-      <p>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>
+      <button onClick={rerollHandler}>Re-roll above</button>
+      <button onClick={() => takeNumber(1)}>Take 1s</button>
+      <button onClick={() => takeNumber(2)}>Take 2s</button>
+      <button onClick={() => takeNumber(3)}>Take 3s</button>
+      <button onClick={() => takeNumber(4)}>Take 4s</button>
+      <button onClick={() => takeNumber(5)}>Take 5s</button>
+      <button onClick={() => takeNumber(6)}>Take 6s</button>
       <div className={styles['die-group']}>
         {Children.toArray(
           heldDice.map((die, index) => (
@@ -66,7 +90,11 @@ const YahtzeeDice = () => {
           ))
         )}
       </div>
-    </>
+      <div className={styles['reset-div']}>
+        <span>{`Count: ${rollCount}`}</span>
+        <button onClick={() => setReset(true)}>Reset</button>
+      </div>
+    </div>
   );
 };
 
